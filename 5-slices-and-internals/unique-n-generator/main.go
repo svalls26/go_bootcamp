077e820e92c2dd247c564bec91bb544258148636
svalls26 <svallsnavarro26@gmail.com>
2023-12-27 21:25:01 +0100

    Slices

---
+++ 5-slices-and-internals/unique-n-generator/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"sort"
+	"strconv"
+	"time"
+)
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	max, _ := strconv.Atoi(os.Args[1])
+
+	var uniques []int
+
+loop:
+	for len(uniques) < max {
+		n := rand.Intn(max) + 1
+		fmt.Print(n, " ")
+
+		for _, u := range uniques {
+			if u == n {
+				continue loop
+			}
+		}
+
+		uniques = append(uniques, n)
+	}
+	fmt.Println("\n\nuniques:", uniques)
+
+	sort.Ints(uniques)
+	fmt.Println("\nsorted:", uniques)
+
+	nums := [5]int{5, 4, 3, 2, 1}
+	sort.Ints(nums[:])
+	fmt.Println("\nnums:", nums)
+}
